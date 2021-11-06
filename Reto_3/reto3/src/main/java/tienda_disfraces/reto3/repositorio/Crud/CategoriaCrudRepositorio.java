3606904635c918e81097965819f0c4de5ff20d5c
BayronThorne <66744260+BayronThorne@users.noreply.github.com>
2021-11-05 21:28:08 -0500

    Add files via upload

---
+++ Reto_3/reto3/src/main/java/tienda_disfraces/reto3/repositorio/Crud/CategoriaCrudRepositorio.java
@@ -0,0 +1,10 @@
+package tienda_disfraces.reto3.repositorio.Crud;
+
+import tienda_disfraces.reto3.modelo.Categoria;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface CategoriaCrudRepositorio extends CrudRepository <Categoria,Integer> {
+    
+}
